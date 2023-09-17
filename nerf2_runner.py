55840f85924d5f8801206d9130ddfbbf6fc97114
ZHAO Xiaopeng <xpzhao@smail.nju.edu.cn>
2023-09-18 00:10:46 +0800

    update for iterations

--- nerf2_runner.py
+++ nerf2_runner.py
@@ -64,11 +64,11 @@ class NeRF2_Runner():
 
 
         ## Train Settings
-        self.current_epoch, self.global_step = 1, 1
+        self.current_iteration = 1
         if kwargs_train['load_ckpt'] or mode == 'test':
             self.load_checkpoints()
         self.batch_size = kwargs_train['batch_size']
-        self.total_epoches = kwargs_train['total_epoches']
+        self.total_iterations = kwargs_train['total_iterations']
         self.save_freq = kwargs_train['save_freq']
 
 
@@ -82,18 +82,16 @@ class NeRF2_Runner():
 
         self.train_iter = DataLoader(train_set, batch_size=self.batch_size, shuffle=True, num_workers=0)
         self.test_iter = DataLoader(test_set, batch_size=self.batch_size, shuffle=True, num_workers=0)
-        self.logger("Train set size:%d, Test set size:%d", len(train_set), len(test_set))
+        self.logger.info("Train set size:%d, Test set size:%d", len(train_set), len(test_set))
 
 
     def load_checkpoints(self):
-        """load checkpoints and epoch
-        """
         ckptsdir = os.path.join(self.logdir, self.expname, 'ckpts')
         if not os.path.exists(ckptsdir):
             os.makedirs(ckptsdir)
         ckpts = [os.path.join(ckptsdir, f) for f in sorted(os.listdir(ckptsdir)) if 'tar' in f]
+        self.logger.info('Found ckpts %s', ckpts)
 
-        print('Found ckpts', ckpts)
         if len(ckpts) > 0:
             ckpt_path = ckpts[-1]
             self.logger.info('Loading ckpt %s', ckpt_path)
@@ -103,37 +101,38 @@ class NeRF2_Runner():
             self.optimizer.load_state_dict(ckpt['optimizer_state_dict'])
             self.cosine_scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer=self.optimizer,T_max=20,eta_min=1e-5)
             self.cosine_scheduler.load_state_dict(ckpt['scheduler_state_dict'])
-            self.current_epoch = ckpt['current_epoch']
-            self.global_step = ckpt['global_step']
+            self.current_iteration = ckpt['current_iteration']
+
 
 
     def save_checkpoint(self):
-        """load checkpoints and epoch
-        """
         ckptsdir = os.path.join(self.logdir, self.expname, 'ckpts')
         model_lst = [x for x in sorted(os.listdir(ckptsdir)) if x.endswith('.tar')]
         if len(model_lst) > 2:
             os.remove(ckptsdir + '/%s' % model_lst[0])
 
-        ckptname = os.path.join(ckptsdir, '{:06d}.tar'.format(self.current_epoch))
+        ckptname = os.path.join(ckptsdir, '{:06d}.tar'.format(self.current_iteration))
         torch.save({
-            'current_epoch': self.current_epoch,
-            'global_step': self.global_step,
+            'current_iteration': self.current_iteration,
             'nerf2_network_state_dict': self.nerf2_network.state_dict(),
             'optimizer_state_dict': self.optimizer.state_dict(),
             'scheduler_state_dict': self.cosine_scheduler.state_dict()
         }, ckptname)
-        self.logger.info('Saved checkpoints at %s', ckptname)
+        # self.logger.info('Saved checkpoints at %s', ckptname)
+        return ckptname
+
 
 
     def train(self):
         """train the model
         """
-
-        self.logger.info("Start training. Current Epoch:%d", self.current_epoch)
-        for epoch in range(self.current_epoch, self.total_epoches + 1):
-            with tqdm(total=len(self.train_iter), desc=f"Epoch {epoch}/{self.total_epoches}") as pbar:
+        self.logger.info("Start training. Current Iteration:%d", self.current_iteration)
+        while self.current_iteration <= self.total_iterations:
+            with tqdm(total=len(self.train_iter), desc=f"Iteration {self.current_iteration}/{self.total_iterations}") as pbar:
                 for train_input, train_label in self.train_iter:
+                    if self.current_iteration > self.total_iterations:
+                        break
+
                     train_input, train_label = train_input.to(self.devices), train_label.to(self.devices)
                     rays_o, rays_d, tx_o = train_input[:, :3], train_input[:, 3:6], train_input[:, 6:9]
                     predict_spectrum = self.renderer.render_ss(tx_o, rays_o, rays_d)
@@ -143,14 +142,17 @@ class NeRF2_Runner():
                     loss.backward()
                     self.optimizer.step()
                     self.cosine_scheduler.step()
-                    self.global_step += 1
+                    self.current_iteration += 1
 
-                    self.writer.add_scalar('Loss/loss', loss, self.global_step)
+                    self.writer.add_scalar('Loss/loss', loss, self.current_iteration)
                     pbar.update(1)
-                    pbar.set_postfix_str('loss = {:.4f}, lr = {:.8f}'.format(loss.item(), self.optimizer.param_groups[0]['lr']))
+                    pbar.set_description(f"Iteration {self.current_iteration}/{self.total_iterations}")
+                    pbar.set_postfix_str('loss = {:.4f}, lr = {:.6f}'.format(loss.item(), self.optimizer.param_groups[0]['lr']))
 
-                if epoch % self.save_freq == 0:
-                    self.save_checkpoint()
+
+                    if self.current_iteration % self.save_freq == 0:
+                        ckptname = self.save_checkpoint()
+                        pbar.write('Saved checkpoints at {}'.format(ckptname))
 
 
 
