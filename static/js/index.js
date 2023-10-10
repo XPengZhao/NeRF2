a77198fd0b17f46c19d72c86a38672107b6ab942
xpzhao <zxpengcat@gmail.com>
2023-10-10 16:44:06 +0800

    update information

--- static/js/index.js
+++ static/js/index.js
@@ -15,7 +15,18 @@ $(document).ready(function() {
 
 		// Initialize all div with carousel class
     var carousels = bulmaCarousel.attach('.carousel', options);
-	
+
     bulmaSlider.attach();
 
 })
+
+function copyCode() {
+	var code = document.querySelector("#BibTeX code").innerText;
+	var tempInput = document.createElement("textarea");
+	tempInput.style = "position: absolute; left: -1000px; top: -1000px";
+	tempInput.value = code;
+	document.body.appendChild(tempInput);
+	tempInput.select();
+	document.execCommand("copy");
+	document.body.removeChild(tempInput);
+  }
